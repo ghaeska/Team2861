4fc0788c8bcc5667bb147c8a3e2f2b4475940b2c
Garten Haeska <ghaeska@gmail.com>
2024-01-28 20:32:36 -0600

    Add auto commands, similar to cranberry alarm

---
+++ 2024 Crescendo/src/main/java/frc/robot/autonomous/tasks/IntakeTask.java
@@ -0,0 +1,33 @@
+package frc.robot.autonomous.tasks;
+
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake.IntakeState;
+import frc.robot.subsystems.Intake.PivotTarget;
+
+public class IntakeTask extends Task 
+{
+  private Intake m_intake;
+  private IntakeState m_intakeState;
+  private PivotTarget m_pivotTarget;
+
+  public IntakeTask(IntakeState intakeState, PivotTarget pivotTarget) {
+    m_intake = Intake.getInstance();
+    m_intakeState = intakeState;
+    m_pivotTarget = pivotTarget;
+  }
+
+  @Override
+  public void start() {
+    m_intake.setState(m_intakeState);
+    m_intake.setPivotTarget(m_pivotTarget);
+  }
+
+  @Override
+  public void update() {
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
