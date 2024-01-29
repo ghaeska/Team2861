a54b39a934b3190cec936b1c65f9bd23bb803d46
Garten Haeska <ghaeska@gmail.com>
2024-01-28 18:46:40 -0600

    Adding Intake/subsystem/helpers to repo

---
+++ 2024 Crescendo/src/main/java/frc/utils/Helpers.java
@@ -0,0 +1,29 @@
+package frc.utils;
+
+public class Helpers 
+{
+  public static double modRotations( double input ) 
+  {
+    input %= 1.0;
+    if( input < 0.0 ) 
+    {
+      input += 1.0;
+    }
+    return input;
+  }
+
+  public static double modDegrees( double input ) 
+  {
+    input %= 360.0;
+    if( input < 0.0 ) 
+    {
+      input += 360.0;
+    }
+    return input;
+  }
+
+  public static int clamp( int val, int min, int max )
+  {
+    return Math.max( min, Math.min( max, val ) );
+  }
+}
