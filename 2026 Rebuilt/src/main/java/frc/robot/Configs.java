3aa1e8635b9ded362925079141c566e17a4fb1c7
Garten Haeska <ghaeska@gmail.com>
2026-01-06 20:50:32 -0600

    Getting baseline code ready for 2026 Season

---
+++ 2026 Rebuilt/src/main/java/frc/robot/Configs.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import frc.robot.SwerveConstants.ModuleConstants;
+import frc.robot.Constants;
+
+public class Configs 
+{
+  /* MAXSwerve Configs */
+
+  /* Subsystem Configs */
+    
+}
--- 2026 Rebuilt/src/main/java/frc/robot/Robot.java
+++ 2026 Rebuilt/src/main/java/frc/robot/Robot.java
@@ -8,35 +8,40 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
- */
+/*
+** The methods in this class are called automatically corresponding to each mode, as described in
+**  the TimedRobot documentation. If you change the name of this class or the package after creating
+**  this project, you must also update the Main.java file in the project.
+*/
 public class Robot extends TimedRobot 
 {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  /*
+  **  This function is run when the robot is first started up and should be used for any
+  **  initialization code.
+  */
   public Robot() 
   {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
+    /* Add USB Cameras here. */
+    // TODO: Add USB Camera here if needed.
+
+    /* Add Limelight Camera Network ports to forward, here. */
+    // TODO: Add limelight cameras here.  Last year we did one, this year we have two!
+
+    /* Init the Robot Container, so our button bindings and Autos are added to the robot. */
     m_robotContainer = new RobotContainer();
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+  /*
+  **  This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+  **  that you want ran during disabled, autonomous, teleoperated and test.
+  ** 
+  **  <p>This runs after the mode specific periodic functions, but before LiveWindow and
+  **  SmartDashboard integrated updating.
+  */
   @Override
   public void robotPeriodic() 
   {
@@ -51,20 +56,19 @@ public class Robot extends TimedRobot
   @Override
   public void disabledInit() 
   {
-
   }
 
   @Override
   public void disabledPeriodic() 
   {
-
   }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() 
   {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    //TODO: Uncomment the following line for auto to work.
+    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) 
@@ -77,7 +81,6 @@ public class Robot extends TimedRobot
   @Override
   public void autonomousPeriodic() 
   {
-
   }
 
   @Override
