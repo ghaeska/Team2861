b3a8c8ef80b883e6652fb8ef562f0f960f1bd2af
Team2861 <team2861@lake-city.k12.mn.us>
2026-01-23 17:03:00 -0600

    adding drive and vision code into project

--- 2026 Rebuilt/src/main/java/frc/robot/Configs.java
+++ 2026 Rebuilt/src/main/java/frc/robot/Configs.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 //import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
@@ -11,7 +13,62 @@ import frc.robot.Constants;
 public class Configs 
 {
   /* MAXSwerve Configs */
+public static final class MAXSwerveModule 
+  {
+    public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
+    public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
 
+    static 
+    {
+      // Use module constants to calculate conversion factors and feed forward gain.
+      double drivingFactor = ModuleConstants.kWheelDiameterMeters * Math.PI
+                / ModuleConstants.kDrivingMotorReduction;
+      double turningFactor = 2 * Math.PI;
+      double nominalVoltage = 12.0;
+            double drivingVelocityFeedForward = nominalVoltage / ModuleConstants.kDriveWheelFreeSpeedRps;
+
+      
+      /* Driving Motor Configs. */
+      drivingConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(50);
+      drivingConfig.encoder
+                .positionConversionFactor(drivingFactor) // meters
+                .velocityConversionFactor(drivingFactor / 60.0); // meters per second
+      drivingConfig.closedLoop
+                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                // These are example gains you may need to them for your own robot!
+                .pid(0.04, 0, 0)
+                .outputRange(-1, 1)
+                .feedForward.kV(drivingVelocityFeedForward);
+                ;
+
+      /* Turning Motor Configs. */
+      turningConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(20);
+      turningConfig.absoluteEncoder
+                // Invert the turning encoder, since the output shaft rotates in the opposite
+                // direction of the steering motor in the MAXSwerve Module.
+                .inverted(true)
+                .positionConversionFactor(turningFactor) // radians
+                .velocityConversionFactor(turningFactor / 60.0) // radians per second
+                // This applies to REV Through Bore Encoder V2 (use REV_ThroughBoreEncoder for V1):
+                 .apply(AbsoluteEncoderConfig.Presets.REV_ThroughBoreEncoderV2);
+
+      turningConfig.closedLoop
+                .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+                // These are example gains you may need to them for your own robot!
+                .pid(1, 0, 0)
+                .outputRange(-1, 1)
+                // Enable PID wrap around for the turning motor. This will allow the PID
+                // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
+                // to 10 degrees will go through 0 rather than the other direction which is a
+                // longer route.
+                .positionWrappingEnabled(true)
+                .positionWrappingInputRange(0, turningFactor);
+        }
   /* Subsystem Configs */
     
+  }
 }
--- 2026 Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026 Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -37,6 +37,8 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import java.util.List;
 
 /* Subsystem Imports */
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Vision.VisionSubsystem;
 
 /* Pathplanner Imports */
 
@@ -54,14 +56,15 @@ public class RobotContainer
 {
   /* Subsytem initilization */
   // TODO: Add Robot subsystems here.  Arms, elevators, vision systems.... also our auto chooser.
-
+  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+  private final VisionSubsystem m_vision = new VisionSubsystem();
 
   /* The controller that are used to control the robot.  Initialized here. */
-  // TODO: Make two xbox controllers.  One for Driver and one for operator.
+  // TODO, Done: Make two xbox controllers.  One for Driver and one for operator.
   // Note: Instead of "OperatorConstants.kDriverControllerPort" we use the following "OIConstants.kDriverControllerPort"
   // Because we have them defined in the OIConstants structure, not the operator structure.
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OIConstants.kDriverControllerPort);
+  CommandXboxController m_DriverController = new CommandXboxController( OIConstants.kDriverControllerPort );
+  CommandXboxController m_OperatorController = new CommandXboxController( OIConstants.k2ndDriverControllerPort );
 
   /* Named Commands used in Auto */
   private void registerNamedCommands()
@@ -83,12 +86,99 @@ public class RobotContainer
 
     /* Add Default Commands here. */
     // TODO: We should need to add the swerve drive default here.
+    m_robotDrive.setDefaultCommand(
+      // The left stick controls translation of the robot.
+      // Turning is controlled by the X axis of the right stick.
+      new RunCommand( () -> m_robotDrive.drive(
+        getDriveForward(),
+        getDriveStrafe(),
+        getDriveRotation(),
+        DriveFieldRelative(),
+        false),
+        m_robotDrive ) );
   }
 
+private boolean DriveFieldRelative()
+  {
+    if( m_DriverController.leftTrigger().getAsBoolean() == true )
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+
+  private Command DriveStop()
+  {
+    return new RunCommand( 
+      () -> m_robotDrive.drive( 
+      0, 
+      0,
+      0, 
+      true, 
+      false), 
+      m_robotDrive );
+  }
+
+  private double getDriveStrafe()
+  {
+    double controllerStrafe = -MathUtil.applyDeadband(m_DriverController.getLeftX(), OIConstants.kDriveDeadband);
+    if( m_DriverController.leftTrigger().getAsBoolean() == true )
+    {
+      //check to see if the area is large enough to assume we are lined up.
+      double m_strafe = m_vision.getLimelightTA();
+      if( m_strafe >= 11.5 )
+      {
+        //we are close to on center, stop allowing strafe movements.
+        controllerStrafe = 0;
+      }      
+    }
+    return controllerStrafe;
+  }
+
+  private double getDriveForward()
+  {
+    double controllerForward = -MathUtil.applyDeadband(m_DriverController.getLeftY(), OIConstants.kDriveDeadband);
+    if( m_DriverController.leftTrigger().getAsBoolean() == true )
+    {
+      double m_fwd = m_vision.limelight_range_proportional();
+      return m_fwd;
+      //return controllerForward;
+    }
+    else 
+    {
+      return controllerForward;
+    }
+  }
+
+  private double getDriveRotation() 
+  {
+    double controllerAngle = -MathUtil.applyDeadband(m_DriverController.getRightX(), OIConstants.kDriveDeadband) ;
+    if( m_DriverController.leftTrigger().getAsBoolean() == true ) 
+    {
+      Double m_rot = m_vision.limelight_aim_proportional();
+      
+      return m_rot;
+      
+    } 
+    else 
+    {
+      return controllerAngle;
+    }
+  }
   /* Configure all the buttons for the controller. */
   private void configureButtonBindings() 
   {
-    
+     /************************* DriveTrain Commands ****************************/
+    /* Command to set wheels in X formation when right stick gets pushed down. */
+    m_DriverController.rightStick().whileTrue( new RunCommand( () -> m_robotDrive.setX(), m_robotDrive ));
+
+    /* Command to reset the robot heading when "start" gets pushed. */
+    m_DriverController.start().onTrue(new InstantCommand( m_robotDrive::zeroHeading ).ignoringDisable(true) );
+    m_OperatorController.start().onTrue( new InstantCommand( m_robotDrive::zeroHeading ).ignoringDisable(true) );
+
   }
 
   /* Send Commands to the Auto Chooser.  */
